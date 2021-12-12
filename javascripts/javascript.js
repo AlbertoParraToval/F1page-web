ea6fd15997bf55a697cc684db13f6c815f713034
Alberto Parra Toval <91873665+AlbertoParraToval@users.noreply.github.com>
2021-12-12 19:28:24 +0100

    Add files via upload

---
+++ javascripts/javascript.js
@@ -0,0 +1,11 @@
+/* BARRA DE NAVEGACION RESPONSIVE*/
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+
